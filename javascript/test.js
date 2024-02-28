9785d62fa09853b3e9e42dcd5ac88a9bf3fc7cfc
madsr <madsrantzau2nd@gmail.com>
2024-02-28 15:38:41 +0100

    nyeste filer, de gamle ligger på samsung pc

--- javascript/test.js
+++
@@ -1,6 +0,0 @@
-function test(){
-    return 2;
-}
-
-let runtest = test();
-console.log(runtest);
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { exec } = require('child_process');
+
+const app = express();
+const PORT = 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Define a route to handle GET requests to the root URL
+app.get('/', (req, res) => {
+    // Send the HTML file when accessing the root URL
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+// Define a route to handle POST requests to execute Python script
+app.post('/execute-python', (req, res) => {
+    const pythonScriptPath = 'main.py';
+
+    exec(`python ${pythonScriptPath}`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error executing Python script: ${error}`);
+            res.status(500).send('Error executing Python script');
+            return;
+        }
+        console.log('Python script output:', stdout);
+        console.error('Python script errors:', stderr);
+        res.sendStatus(200);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
