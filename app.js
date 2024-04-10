9f3baa6cdfcfe0b3f1eb9375dc612b145628d213
MadsRantzau49 <madsrantzau2nd@gmail.com>
2024-04-10 16:02:39 +0200

    maybe the right path

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.post('/execute-python', (req, res) => {
   const pythonScriptPath = path.join(__dirname,'python', 'main.py');
 
 
-  exec(`python3 /opt/render/project/src/python/main.py`, (error, stdout, stderr) => {
+  exec(`/opt/render/project/src/myenv/bin/python3 /opt/render/project/src/python/main.py`, (error, stdout, stderr) => {
       if (error) {
           console.error(`Error executing Python script: ${error}`);
           res.status(500).send('Error executing Python script');
