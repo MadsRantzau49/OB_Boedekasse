bcad4d5820c13dfb080b3ac75733fe5bc17e95e8
MadsRantzau49 <madsrantzau2nd@gmail.com>
2024-04-10 15:58:40 +0200

    python path

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.post('/execute-python', (req, res) => {
   const pythonScriptPath = path.join(__dirname,'python', 'main.py');
 
 
-  exec(`/opt/render/.local/bin/python3 /opt/render/project/src/python/main.py`, (error, stdout, stderr) => {
+  exec(`python3 /opt/render/project/src/python/main.py`, (error, stdout, stderr) => {
       if (error) {
           console.error(`Error executing Python script: ${error}`);
           res.status(500).send('Error executing Python script');
