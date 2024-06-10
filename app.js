043833d1b0e01616eca0a0bba8021847c73f439c
MadsRantzau49 <madsrantzau2nd@gmail.com>
2024-06-10 15:00:46 +0200

    trans

--- app.js
+++ app.js
@@ -1,45 +1,7 @@
-// var createError = require('http-errors');
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-// const fs = require("fs");
-// const { exec } = require('child_process');
-
-
-// var app = express();
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// // app.use('/', indexRouter);
-// // app.use('/users', usersRouter);
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // send JSON response
-//   res.status(err.status || 500);
-//   res.json({ error: err.message });
-// });
-
 const express = require('express');
 const { exec } = require('child_process');
 const path = require('path');
 const fs = require("fs");
-
-
 
 const app = express();
 const PORT = 10000;
